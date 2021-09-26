98f32c13bce9e25188daa95dc158fad0533dfd65
fatihk25 <fatihkhoiri25@gmail.com>
2021-09-26 18:46:08 +0700

    Update Login Register Controller

--- app/Http/Controllers/ConsultantController.php
+++ app/Http/Controllers/ConsultantController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Consultant;
+use Exception;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -20,13 +21,13 @@ class ConsultantController extends Controller
 
     public function register(Request $request)
     {
-        $this->validate($request,[
+        try {
+            $request->validate([
             'name' => ['required','string'],
             'email' => ['required','email','unique:consultants'],
             'password' => ['required'],
-        ]);
+            ]);
 
-        try {
             $data = new Consultant;
             $data->name = $request->input('name');
             $data->email = $request->input('email');
@@ -49,24 +50,28 @@ class ConsultantController extends Controller
 
     public function login(Request $request) 
     {
-        $email = $request->input('email');
-        $password = $request->input('password');
 
-        $data = Consultant::where('email', $email)->first();
-
-        if(Hash::check($password, $data->password))
-        {
+        try {
+            $request->validate([
+                'email' => ['required','email'],
+                'password' => ['required'],
+            ]);
+            $email = $request->input('email');
+            $password = $request->input('password');
+            $data = Consultant::where('email', $email)->first();
+    
+            if(Hash::check($password, $data->password))
+            {
+                return response()->json([
+                    'status' => 'success',
+                    'message' => 'login successfully',
+                    'data' => $data
+                ],201);
+            }
+        }catch(\Exception $e){
             return response()->json([
-                'status' => 'success',
-                'message' => 'login successfully',
-                'data' => $data
-            ],201);
-        }else
-        {
-            return response()->json([
-                'success' => false,
-                'message' => 'Login failed',
-                'data' => ''
+                'status' => 'failed',
+                'message' => 'Login failed'
             ]);
         }
     }

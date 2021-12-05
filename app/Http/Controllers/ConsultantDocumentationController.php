10acbffcc680d7c9179b9db4e1b54750d7cf97e7
fatihk25 <fatihkhoiri25@gmail.com>
2021-12-05 21:52:45 +0700

    Update consultant documentation

--- app/Http/Controllers/ConsultantDocumentationController.php
+++ app/Http/Controllers/ConsultantDocumentationController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Consultant;
 use App\ConsultantDocumentation;
+use App\Http\Resources\ConsultantDocumentationResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Str;
 
 class ConsultantDocumentationController extends Controller
 {
@@ -12,6 +15,49 @@ class ConsultantDocumentationController extends Controller
     public function __construct()
     {
         $this->middleware('auth:consultants-api');
+    }
+
+    public function upload(Request $request, $id) {
+        if(Gate::denies('update-data-consultant', (int)$id)) {
+            return response()->json([
+                'code' => 403,
+                'message' => 'Forbidden'
+            ],403);
+        }
+        
+        $request->validate([
+            'consultation_doc.id' => ['integer'],
+            'consultation_doc.photo' =>  ['mimes:jpg,png,jpeg', 'max:1024']
+        ]);
+
+        foreach($request->consultation_doc as $doc) {
+            if($doc["id"] < 0) {
+                $consultantDoc = new ConsultantDocumentation();
+                $consultantDoc->consultant_id = $id;
+                $imageName = Str::random(50) . "." .  $doc["photo"]->extension();
+                $path = public_path('images/');
+                $doc["photo"]->move($path, $imageName);
+                $image = "images/" . $imageName;
+                $consultantDoc->photo = $image;
+                $consultantDoc->save();
+            } else {
+                $consultantDoc = ConsultantDocumentation::find($doc["id"]);
+                $consultantDoc->consultant_id = $id;
+                $imageName = Str::random(50) . "." .  $doc["photo"]->extension();
+                $path = public_path('images/');
+                $doc["photo"]->move($path, $imageName);
+                $image = "images/" . $imageName;
+                $consultantDoc->photo = $image;
+                $consultantDoc->save();
+            }
+        }
+
+        $data = Consultant::findOrFail($id);
+
+        return response()->json([
+            'code' => 200,
+            'data' => ConsultantDocumentationResource::collection($data->documentation)
+        ], 200);
     }
 
     public function destroy(Request $request, $id) {
